46f5d2d63e83247e1dbdb88677b518dcfa9a9539
Evan Lucas <evanlucas@me.com>
2013-11-17 15:45:22 -0600

    Added actions for commands: create-user, add-key, list-users, rm-key, and list

--- bin/mapps.js
+++ bin/mapps.js
@@ -3,6 +3,7 @@ var program     = require('commander')
   , pkg         = require('../package')
   , mapps       = require('../lib/index')
   , inquirer    = require('inquirer')
+  , Table       = require('cli-table')
 
 program
   .command('serve')
@@ -57,24 +58,100 @@ program
   })
 
 program
-  .command('add-key <key>')
+  .command('add-key')
   .description('Adds an API key')
-  .action(function(k) {
+  .action(function() {
+    var q = [
+      {type: 'input', name: 'key', message: 'Please enter the API key'},
+      {type: 'input', name: 'name', message: 'Please enter the user\'s name'},
+      {type: 'input', name: 'email', message: 'Please enter the user\'s email'}
+    ]
+    inquirer.prompt(q, function(a) {
+      mapps.addUser(a.name, a.email, a.key, function(err, user) {
+        if (err) {
+          mapps.log.error('add-key', 'Error adding user with key', err)
+          process.exit(1)
+        }
+        mapps.log.info('add-key', 'Created user', user)
+        process.exit()
+      })
+    })
+  })
 
+program
+  .command('list-users')
+  .description('Lists all users')
+  .action(function() {
+    mapps.listUsers(function(err, users) {
+      if (err) {
+        mapps.log.error('list-users', 'Error listing users', err)
+        process.exit(1)
+      }
+      var table = new Table({
+        head: ['Name', 'Email', 'AuthToken']
+      })
+      users.forEach(function(user) {
+        table.push([ user.name || '', user.email || '', user.authToken])
+      })
+      console.log(table.toString())
+      process.exit()
+    })
   })
 
 program
   .command('rm-key <key>')
   .description('Removes an API key')
   .action(function(k) {
-
+    mapps.removeUserWithKey(k, function(err) {
+      if (err) {
+        mapps.log.error('rm-key', 'Error removing user with key', err)
+        process.exit(1)
+      }
+      process.exit()
+    })
   })
 
 program
   .command('list [name|label]')
   .description('List registered apps')
-  .action(function() {
-
+  .action(function(nl) {
+    mapps.listApps(nl, function(err, apps) {
+      if (err) {
+        mapps.log.error('list', 'Error listing app', err)
+        process.exit(1)
+      }
+      if (nl) {
+        var table = new Table({
+          head: ['Key', 'Value']
+        })
+        var keys = Object.keys(apps)
+        keys.forEach(function(key) {
+          if (key !== 'config') {
+            table.push([key, apps[key]])
+          }
+        })
+        
+        if (apps.config) {
+          var keys = Object.keys(apps.config)
+          keys.forEach(function(k) {
+            table.push([k, apps.config[k]])
+          })
+        }
+        console.log(table.toString())
+        process.exit()
+      } else {
+        var table = new Table({
+          head: ['Name', 'Label', 'Port', 'Status']
+        })
+        apps.forEach(function(app) {
+          mapps.log.info('App', app)
+          table.push([ app.name, app.label || '', app.port, app.config.PID || app.config.pid || 'Not Running'])
+  //        table.push([ app.name, app.label || '', app.port, app.config.PID])
+        })
+        console.log(table.toString())
+        process.exit()
+      }
+    })
   })
 
 program
@@ -98,8 +175,6 @@ program
 
   })
 
-program
-  .command('start')
 
 program
   .version(pkg.version)
